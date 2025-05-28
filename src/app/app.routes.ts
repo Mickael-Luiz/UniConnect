b2474a58d95dcd2a0ce7c2c99317e36306cda9b7
Mickael Dias <mickaeldias00@gmail.com>
2025-05-27 23:26:54 -0400

    Criação Primeira Tela Bem Vindo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,14 @@ import { LoginOptionsComponent } from './pages/login-options/login-options.compo
 import { RegisterOptionsComponent } from './pages/register-options/register-options.component';
 import { RegistroProfessorComponent } from './pages/register-options/registro-professor/registro-professor.component';
 import { RegistroFaculdadeComponent } from './pages/register-options/registro-faculdade/registro-faculdade.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
 
 export const routes: Routes = [
   {path: '', component: WelcomeComponent},
   {
-    path: 'login', children: [
+    path: 'login', component: AuthLayoutComponent,
+    children: [
       {path: '', component: LoginOptionsComponent},
       {path: 'aluno', component: LoginAlunoComponent},
       {path: 'professor', component: LoginProfessorComponent},
@@ -19,13 +22,18 @@ export const routes: Routes = [
     ]
   },
   {
-    path: 'registro',
+    path: 'registro', component: AuthLayoutComponent,
     children: [
       {path: '', component: RegisterOptionsComponent},
       {path: 'aluno', component: LoginAlunoComponent},
       {path: 'professor', component: RegistroProfessorComponent},
       {path: 'faculdade', component: RegistroFaculdadeComponent},
     ]
-  }
+  },
+  {
+    path: 'app', component: AppLayoutComponent,
+    children: []
+  },
+  {path: '**', redirectTo: ''}
 
 ];
