b4f713b5450a676ae68c366688400697b0ff3056
Mickael Dias <mickaeldias00@gmail.com>
2025-05-28 23:33:45 -0400

    Ajustes gerais nas cores

---
+++ src/app/mocks/mock-users.ts
@@ -0,0 +1,20 @@
+export const MOCK_USERS = [
+  {
+    id: 1,
+    nome: 'Faculdade EdInsina',
+    email: 'faculdade@edinsina.com',
+    tipo: 'FACULDADE'
+  },
+  {
+    id: 2,
+    nome: 'Ed Wilson JR',
+    email: 'edficil@gmail.com',
+    tipo: 'PROFESSOR'
+  },
+  {
+    id: 3,
+    nome: 'Igor Ila Kong',
+    email: 'igorilakong@gmail.com',
+    tipo: 'ALUNO'
+  }
+]
