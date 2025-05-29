4345342571e8d4ae445de2603be4638f67d1bc3e
Mickael Dias <mickaeldias00@gmail.com>
2025-05-29 17:27:52 -0400

    Função de login para usuários (apenas email)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  constructor(private router: Router) {}
+  formUsuario: FormGroup;
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private fb: FormBuilder
+  ) {
+    this.formUsuario = this.fb.group({
+      email: ['', Validators.required],
+      senha: [''],
+      manterConectado: ['']
+    })
+  }
 
   navigateTo(url: string) {
     this.router.navigate([url]);
+  }
+
+  logar(e: Event) {
+    e.preventDefault()
+    console.log(this.formUsuario.value);
+    this.auth.login(this.formUsuario.value.email)
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { MOCK_USERS } from '../mocks/mock-users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly storageKey = 'usuario-logado';
+
+  constructor() { }
+
+  login(email: string): boolean {
+    const user = MOCK_USERS.find(u => u.email == email);
+    if(user) {
+      localStorage.setItem(this.storageKey, JSON.stringify(user));
+      return true;
+    }
+    return false;
+  }
+
+  logout() {
+    localStorage.removeItem(this.storageKey);
+  }
+
+  getUsuario() {
+    const user = localStorage.getItem(this.storageKey);
+    return user ? JSON.parse(user) : null;
+  }
+
+  getPerfil() {
+    const user = this.getUsuario();
+    return user ? user.tipo : null;
+  }
+
+  isFaculdade() {
+    return this.getPerfil() === 'FACULDADE';
+  }
+
+  isAluno() {
+    return this.getPerfil() === 'ALUNO';
+  }
+
+  isProfessor() {
+    return this.getPerfil() === 'PROFESSOR';
+  }
+
+  isLogado() {
+    return !!this.getUsuario();
+  }
+}
