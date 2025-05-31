bf82901a0c7caa405d76510f2ae0987fbf9eaa7c
Mickael Dias <mickaeldias00@gmail.com>
2025-05-31 01:59:09 -0400

    Leftbar

---
+++ src/app/layouts/main-layout/leftbar/leftbar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-leftbar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './leftbar.component.html',
+  styleUrl: './leftbar.component.scss'
+})
+export class LeftbarComponent {
+
+  perfil: string = '';
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.perfil = this.auth.getPerfil();
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate([''])
+  }
+
+}
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from "./topbar/topbar.component";
+import { LeftbarComponent } from "./leftbar/leftbar.component";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, TopbarComponent],
+  imports: [RouterOutlet, TopbarComponent, LeftbarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
